781d47efb8542970695ded2321d786554044d587
MaxBerezhnoi <92980454+MaxBerezhnoi@users.noreply.github.com>
2022-05-10 21:12:17 +0300

    8.2 & 8.3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import player from '@vimeo/player';
+import throttle from 'lodash.throttle'
+
+const iframe = document.querySelector('iframe');
+const Player = new Vimeo.Player(iframe);
+const CURRENT_TIME = 'videoplayer-current-time';
+const onPlay = function(data){
+      localStorage.setItem(CURRENT_TIME, data.seconds);
+};
+Player.on('timeupdate', throttle(onPlay, 1000));
+
+const playedTime = localStorage.getItem(CURRENT_TIME);
+
+Player.setCurrentTime(Number(playedTime)).then(function (seconds) {
+  seconds = Number(playedTime);
+});
+
+
+
 
